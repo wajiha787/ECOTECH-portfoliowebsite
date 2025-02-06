a4a255b3070524ded337d2108b6bb8633d295869
wajiha787 <wajihawaqar1234@gmail.com>
2025-02-06 13:26:38 +0500

    Header done

---
+++ src/Components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../App.css';  
+import imglogo from '../images/ecologo.png'
+function Header() {
+  return (
+    <header>
+      <img className="ecologo" src={imglogo} alt="ECO Tech Fusion Logo" />; 
+      <div className="logo">
+        <span className="ECO">ECO</span>
+        <span className="TECH">TECH</span>
+        <span className="FUSION">FUSION</span>
+        <span className="innovations">Serving Innovations</span>
+      </div>
+      <nav> 
+        <span className="home">Home</span>
+        <span className="solutions">Solution</span>
+        <span className="clients">Clients</span>
+        <span className="aboutus">About Us</span>
+        <button className="contactus">Contact Us</button>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import imgfrontpage from '../images/frontpage.jpg';
+import '../App.css';
+
+const Home = () => {
+    return (
+        <div className="image-container">
+          <img className="frontpage" src={imgfrontpage} alt="ECO Tech front page" />
+          <div className="image-text">
+            Empower your business with innovative IT solutions—where technology meets sustainability and efficiency for a smarter, secure future!
+          </div>
+          <div className="vertical-text">MON-SAT</div>
+          <div className="vertical-text-time">9AM  TO  6PM</div>
+        </div>
+    );
+  };
+  
+export default Home;
